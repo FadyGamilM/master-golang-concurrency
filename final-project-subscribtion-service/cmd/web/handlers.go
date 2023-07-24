f36259ffb8dcb37e037640bb748aee4a8eda98a8
[FadyGamilM] <1700961@eng.asu.edu.eg>
2023-07-24 18:14:23 +0300

    FEATURE | writing stubs for other handlers

--- final-project-subscribtion-service/cmd/web/handlers.go
+++ final-project-subscribtion-service/cmd/web/handlers.go
@@ -24,3 +24,78 @@ func (app *Config) HomeHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(int(res.Status))
 	json.NewEncoder(w).Encode(res)
 }
+
+func (app *Config) LoginHandler(w http.ResponseWriter, r *http.Request) {
+	user := User{
+		Id:   1,
+		Name: "fady",
+	}
+	res := AppResponse{
+		Response: &user,
+		Status:   http.StatusOK,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(int(res.Status))
+	json.NewEncoder(w).Encode(res)
+}
+
+func (app *Config) LogoutHandler(w http.ResponseWriter, r *http.Request) {
+	user := User{
+		Id:   1,
+		Name: "fady",
+	}
+	res := AppResponse{
+		Response: &user,
+		Status:   http.StatusOK,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(int(res.Status))
+	json.NewEncoder(w).Encode(res)
+}
+
+func (app *Config) RegisterHandler(w http.ResponseWriter, r *http.Request) {
+	user := User{
+		Id:   1,
+		Name: "fady",
+	}
+	res := AppResponse{
+		Response: &user,
+		Status:   http.StatusOK,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(int(res.Status))
+	json.NewEncoder(w).Encode(res)
+}
+
+func (app *Config) BuySubscribtionHandler(w http.ResponseWriter, r *http.Request) {
+	user := User{
+		Id:   1,
+		Name: "fady",
+	}
+	res := AppResponse{
+		Response: &user,
+		Status:   http.StatusOK,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(int(res.Status))
+	json.NewEncoder(w).Encode(res)
+}
+
+func (app *Config) ActivateAccountHandler(w http.ResponseWriter, r *http.Request) {
+	user := User{
+		Id:   1,
+		Name: "fady",
+	}
+	res := AppResponse{
+		Response: &user,
+		Status:   http.StatusOK,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(int(res.Status))
+	json.NewEncoder(w).Encode(res)
+}
--- final-project-subscribtion-service/cmd/web/routes.go
+++ final-project-subscribtion-service/cmd/web/routes.go
@@ -14,9 +14,16 @@ func (app *Config) routes() http.Handler {
 
 	// inject middlewares
 	mux.Use(middleware.Recoverer)
+	// inject the session loading and saving middleware, the next handler will be called after loading and before saving so the next handler can modify the request sessions before saving it
+	mux.Use(app.SessionLoad)
 
 	// register endpoints to be handlerd
 	mux.Get("/", app.HomeHandler)
+	mux.Post("/login", app.LoginHandler)
+	mux.Post("/logout", app.LogoutHandler)
+	mux.Post("/register", app.RegisterHandler)
+	mux.Post("/buy-subscription", app.BuySubscribtionHandler)
+	mux.Post("/activate-account", app.ActivateAccountHandler)
 
 	// return the defined router handler mux
 	return mux
