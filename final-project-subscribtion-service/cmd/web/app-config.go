c7774f8b5d47f55d3ff480d42b4e9aaf465d22b4
[FadyGamilM] <1700961@eng.asu.edu.eg>
2023-07-24 17:07:41 +0300

    FEATURE | server is responding

--- final-project-subscribtion-service/cmd/web/app-config.go
+++ final-project-subscribtion-service/cmd/web/app-config.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net/http"
 	"sync"
 
 	"github.com/alexedwards/scs/v2"
@@ -15,4 +16,13 @@ type Config struct {
 	InfoLog *log.Logger
 	ErrLog  *log.Logger
 	wait    *sync.WaitGroup
+}
+
+func (app *Config) is_authenticated(r *http.Request) bool {
+	return app.Session.Exists(r.Context(), "userID")
+}
+
+type AppResponse struct {
+	Response interface{}
+	Status   int32
 }
--- final-project-subscribtion-service/cmd/web/handlers.go
+++ final-project-subscribtion-service/cmd/web/handlers.go
@@ -1,5 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
-func (app *Config) HomeHandler(w http.ResponseWriter, r *http.Request) {}
+type User struct {
+	Id   int
+	Name string
+}
+
+func (app *Config) HomeHandler(w http.ResponseWriter, r *http.Request) {
+	user := User{
+		Id:   1,
+		Name: "fady",
+	}
+	res := AppResponse{
+		Response: &user,
+		Status:   http.StatusOK,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(int(res.Status))
+	json.NewEncoder(w).Encode(res)
+}
